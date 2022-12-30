9981ddafd4587ca07a32ce8e07ab6d68c6dcd6a3
abhi1988sharma <60963480+abhi1988sharma@users.noreply.github.com>
2022-12-30 14:43:39 +0000

    Deploying to gh-pages from @ abhi1988sharma/toDoList@7e8c1ae08b8771617834a22b75a938f4bb256df4 🚀

---
+++ precache-manifest.6658e57761374d26672024a6ac966084.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1668a2a4b66f0939e3c3a3804f282744",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d9940706f2ea1386ac3",
+    "url": "./static/js/2.7dc97f8c.chunk.js"
+  },
+  {
+    "revision": "e2ab4e5e163f2cb9bda1",
+    "url": "./static/js/main.b255baed.chunk.js"
+  },
+  {
+    "revision": "0258c7828350b9a2f194",
+    "url": "./static/js/runtime-main.9fdd2007.js"
+  }
+]);
